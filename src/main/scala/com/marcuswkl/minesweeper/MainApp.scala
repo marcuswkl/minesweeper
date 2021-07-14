4899125a3ee0343d4316269a91c3add53be2dcff
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-15 00:43:06 +0800

    Implement RootLayout in MainApp

--- src/main/scala/com/marcuswkl/minesweeper/MainApp.scala
+++ src/main/scala/com/marcuswkl/minesweeper/MainApp.scala
@@ -1,7 +1,28 @@
 package com.marcuswkl.minesweeper
 
+import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import javafx.scene.layout.BorderPane
+import scalafxml.core.{FXMLLoader, NoDependencyResolver}
 
 object MainApp extends JFXApp {
-  println("Hello World!")
+  // Transform the path of RootLayout.fxml to URI for resource location
+  val rootResource = getClass.getResourceAsStream("view/RootLayout.fxml")
+  // Initialise the loader object
+  val loader = new FXMLLoader(null, NoDependencyResolver)
+  // Load root layout from fxml file
+  loader.load(rootResource);
+  // Retrieve the root component BorderPane from the FXML
+  val roots = loader.getRoot[BorderPane]
+
+  // Initialise stage
+  stage = new PrimaryStage {
+    title = "Minesweeper"
+    scene = new Scene {
+      root = roots
+    }
+    // icons += new Image(getClass.getResourceAsStream(""))
+  }
 }
