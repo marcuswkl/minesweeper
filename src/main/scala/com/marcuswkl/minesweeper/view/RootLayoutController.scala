00798731c53239d4c72f0627fb881c9495fb405a
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-18 02:17:44 +0800

    Update about dialog

--- src/main/scala/com/marcuswkl/minesweeper/view/RootLayoutController.scala
+++ src/main/scala/com/marcuswkl/minesweeper/view/RootLayoutController.scala
@@ -17,9 +17,13 @@ class RootLayoutController() {
   def handleAbout() {
     new Alert(AlertType.Information){
       initOwner(MainApp.stage)
-      title       = "About"
-      headerText  = "Minesweeper"
-      contentText = "Copyrighted by Marcus Wong Ke Lun"
+      title       = "Minesweeper"
+      headerText  = "About Minesweeper"
+      contentText = "Minesweeper is a single-player puzzle video game. The objective of the game is to clear a " +
+        "rectangular board containing hidden \"mines\" or bombs without detonating any of them, with help from clues" +
+        " about the number of neighbouring mines in each field. The game originates from the 1960s, and it has been" +
+        " written for many computing platforms in use today. It has many variations and offshoots. - Wikipedia\n\n" +
+        "This version is copyrighted by Marcus Wong Ke Lun."
     }.showAndWait()
   }
 }
