055b99becbcdbf143b4adf09dd76e8beb76c4c43
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-20 18:57:15 +0800

    Add update mode emoji and rename old method to update tile mode emoji

--- src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
+++ src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
@@ -3,8 +3,19 @@ package com.marcuswkl.minesweeper.model
 import scalafx.scene.image.Image
 
 class EmojiButton(var emoji: Image) {
-  // Update emoji when user clicks a tile
-  def updateEmoji(tileType: String): Unit = {
+  // Update emoji when user changes mode
+  def updateModeEmoji(mode: String): Unit = {
+    if (mode == "tile") {
+      emoji = EmojiButton.emojiSmile
+    } else if (mode == "flag") {
+      emoji = EmojiButton.emojiFlag
+    } else {
+      emoji = EmojiButton.emojiQuestionMark
+    }
+  }
+
+  // Update emoji when user clicks a tile in tile mode
+  def updateTileModeEmoji(tileType: String): Unit = {
     if (tileType == "empty") {
       emoji = EmojiButton.emojiSmile
     } else if (tileType == "number") {
@@ -13,15 +24,16 @@ class EmojiButton(var emoji: Image) {
       emoji = EmojiButton.emojiDead
     }
   }
+
 }
 
 // Companion object to store emoji images
 object EmojiButton {
-  // Tile mode emojis
+  // Mode emojis
   val emojiSmile = new Image(getClass.getResourceAsStream("../images/emoji-smile.png"))
-  val emojiOpenMouth = new Image(getClass.getResourceAsStream("../images/emoji-open-mouth.png"))
-  val emojiDead = new Image(getClass.getResourceAsStream("../images/emoji-dead.png"))
-  // Marker mode emojis
   val emojiFlag = new Image(getClass.getResourceAsStream("../images/emoji-flag.png"))
   val emojiQuestionMark = new Image(getClass.getResourceAsStream("../images/emoji-question-mark.png"))
+  // Tile mode emojis
+  val emojiOpenMouth = new Image(getClass.getResourceAsStream("../images/emoji-open-mouth.png"))
+  val emojiDead = new Image(getClass.getResourceAsStream("../images/emoji-dead.png"))
 }
