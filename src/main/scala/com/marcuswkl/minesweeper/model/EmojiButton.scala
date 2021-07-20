38fe08a7474f17f5efd5861a74c864ce08eca1b5
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-20 18:47:00 +0800

    Add marker emoji assets and companion object variables

--- src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
+++ src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
@@ -17,7 +17,11 @@ class EmojiButton(var emoji: Image) {
 
 // Companion object to store emoji images
 object EmojiButton {
+  // Tile mode emojis
   val emojiSmile = new Image(getClass.getResourceAsStream("../images/emoji-smile.png"))
   val emojiOpenMouth = new Image(getClass.getResourceAsStream("../images/emoji-open-mouth.png"))
   val emojiDead = new Image(getClass.getResourceAsStream("../images/emoji-dead.png"))
+  // Marker mode emojis
+  val emojiFlag = new Image(getClass.getResourceAsStream("../images/emoji-flag.png"))
+  val emojiQuestionMark = new Image(getClass.getResourceAsStream("../images/emoji-question-mark.png"))
 }
