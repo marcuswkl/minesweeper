c5895cdf019b6d6df159a4afe6efe050b5363587
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-20 17:18:16 +0800

    Implement update tile marked status methods for all markers

--- src/main/scala/com/marcuswkl/minesweeper/model/FlagMarker.scala
+++ src/main/scala/com/marcuswkl/minesweeper/model/FlagMarker.scala
@@ -1,5 +1,16 @@
 package com.marcuswkl.minesweeper.model
 
 class FlagMarker(val symbol: String = "🚩") extends Marker() {
+  // Update the flag mark status of the tile
+  override def updateTileMarkedStatus(tile: Tile, isMarked: Boolean): Unit = {
+    tile.isFlagMarked = isMarked
+  }
 
+  // Update the old mark status and new flag mark status of the tile
+  override def updateTileMarkedStatus(tile: Tile, oldMarked: Boolean, newMarked: Boolean): Unit = {
+    if (tile.isQuestionMarked) {
+      tile.isQuestionMarked = oldMarked
+    }
+    tile.isFlagMarked = newMarked
+  }
 }
--- src/main/scala/com/marcuswkl/minesweeper/model/QuestionMarkMarker.scala
+++ src/main/scala/com/marcuswkl/minesweeper/model/QuestionMarkMarker.scala
@@ -1,5 +1,16 @@
 package com.marcuswkl.minesweeper.model
 
 class QuestionMarkMarker(val symbol: String = "?") extends Marker() {
+  // Update the flag mark status of the tile
+  override def updateTileMarkedStatus(tile: Tile, isMarked: Boolean): Unit = {
+    tile.isQuestionMarked = isMarked
+  }
 
+  // Update the old mark status and new question mark status of the tile
+  override def updateTileMarkedStatus(tile: Tile, oldMarked: Boolean, newMarked: Boolean): Unit = {
+    if (tile.isFlagMarked) {
+      tile.isFlagMarked = oldMarked
+    }
+    tile.isQuestionMarked = newMarked
+  }
 }
