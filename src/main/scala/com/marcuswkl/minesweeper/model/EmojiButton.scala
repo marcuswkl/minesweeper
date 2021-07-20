2687dc4256fcaba69cfb2fe7546d51fc686f64dc
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-07-20 17:02:01 +0800

    Improve emoji button update to include UI update

--- src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
+++ src/main/scala/com/marcuswkl/minesweeper/model/EmojiButton.scala
@@ -1,16 +1,19 @@
 package com.marcuswkl.minesweeper.model
 
-import scalafx.scene.image.Image
+import scalafx.scene.image.{Image, ImageView}
 
 class EmojiButton(var emoji: Image) {
   // Update emoji when user clicks a tile
-  def updateEmoji(tileType: String): Unit = {
+  def updateEmoji(tileType: String, emojiButton: ImageView): Unit = {
     if (tileType == "empty") {
       emoji = EmojiButton.emojiSmile
+      emojiButton.setImage(emoji)
     } else if (tileType == "number") {
       emoji = EmojiButton.emojiOpenMouth
+      emojiButton.setImage(emoji)
     } else {
       emoji = EmojiButton.emojiDead
+      emojiButton.setImage(emoji)
     }
   }
 }
